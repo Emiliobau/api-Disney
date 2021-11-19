de4c75fb9baa0d4e8810e1a9acdf8d99ae2da7fc
Emiliobau <emiliobau@hotmail.com>
2021-11-19 02:21:42 -0300

    documentacion endpoint api movie/create

--- src/routers/routerMovie.js
+++ src/routers/routerMovie.js
@@ -3,8 +3,83 @@ const router = express.Router();
 const controller = require("../controllers/controllerMovie")
 const isAuthenticated = require ("../middleware/authToken")
 
+// configuracion documentacion swagger
+
+/**
+ * @swagger
+ *
+ *  components:
+ *    schemas:
+ *      Movie:
+ *        type: object
+ *        properties:
+ *          id:
+ *            type: integer
+ *            description: id usuario
+ *          title:
+ *            type: string
+ *            description: Titulo de la pelicula
+ *          image:
+ *            type: string
+ *            description: path de la imagen de la pelicula
+ *          create_date:
+ *            type: string
+ *            description: fecha de creacion de la pelicula
+ *          rating:
+ *            type: integer
+ *            description: calificacion de la pelicula de 0 a 10
+ *          genre_id:
+ *            type: integer
+ *            description: numero de id del genero de la pelicula
+ *        required:
+ *          -title
+ *          -create_date
+ *          -rating
+ *          -genre_id
+ *        example:
+ *          title: Coco
+ *          image: www.coco.jpg
+ *          create_daye: 2017-11-22
+ *          rating: 9
+ *          genre_id: 6
+ */
+
 
 router.post("/create", isAuthenticated, controller.create)
+/**
+ * @swagger
+ *
+ * /movie/create:
+ *    post:
+ *      summary: creacion de pelicula
+ *      parameters:
+ *        - in: header
+ *          name: Token
+ *          description: token authorization header
+ *          required: true
+ *          type: string
+ *      tags: [Movie]
+ *      requestBody:
+ *        required: true
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: object
+ *              $ref: "#/components/schemas/Movie"
+ *            example:
+ *              title: Coco
+ *              image: www.coco.jpg
+ *              create_date: 2017-11-22
+ *              rating: 9
+ *              genre_id: 6
+ *
+ *      responses:
+ *        200:
+ *          description: Pelicula creada correctamente
+ *        400:
+ *          description: No tiene permiso para crear pelicula
+ */
+
 router.put("/edit/:id", isAuthenticated, controller.update)
 router.delete("/delete/:id", isAuthenticated, controller.delete)
 router.get("/movies",isAuthenticated, controller.list)
--- src/routers/routerUser.js
+++ src/routers/routerUser.js
@@ -47,14 +47,43 @@ const controller = require("../controllers/controllerUser")
  *            schema:
  *              type: object
  *              $ref: "#/components/schemas/User"
+ *            example:
+ *               name: Emilio
+ *               email: emiliobaudracco@gmail.com
+ *               password: emilio123
  *      responses:
  *        200:
  *          description: nuevo usuario creado
  */
 
-
-
 router.post("/register", controller.register)
+
+/**
+ * @swagger
+ *  /auth/login:
+ *    post:
+ *      summary: login Usuario registrado, entrega Token
+ *      tags: [User]
+ *      requestBody:
+ *        required: true
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: object
+ *              $ref: "#/components/schemas/User"
+ *            example:
+ *               email: emiliobaudracco@gmail.com
+ *               password: emilio123
+ *      responses:
+ *        200:
+ *          description: contraseña correcta, Token
+ *        400:
+ *          description: credenciales Invalidas
+ *
+ *
+ */
+
+
 router.post("/login", controller.login)
 
 
